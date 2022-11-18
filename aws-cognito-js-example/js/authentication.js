69a21d7e9e76c8f1025cfd9bf53d7a29317c8150
shachar.doron@gmail.com <Doron1971!!!>
2022-11-18 10:14:48 +0200

    test

--- aws-cognito-js-example/js/authentication.js
+++ aws-cognito-js-example/js/authentication.js
@@ -1,10 +1,12 @@
 
+//origin                
 function getPoolData(){
-	return {
-			UserPoolId: localStorage["aws-congnito-user-pool-id"],
-			ClientId: localStorage["aws-congnito-app-id"] 
-		};
+return {
+		UserPoolId: localStorage["aws-congnito-user-pool-id"],
+		ClientId: localStorage["aws-congnito-app-id"] 
+	};
 }
+
 
 var userPool;
 
